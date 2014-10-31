8620336081f0fd2f5cf7d7b3f9da2144a601ae83
Fredrik Liljegren <fredrik.liljegren@textalk.se>
2014-10-31 22:57:09 +0100

    Updating site app.

--- app.rb
+++ app.rb
@@ -1,33 +1,78 @@
 # encoding: utf-8
-require 'sinatra'
-require "sinatra/reloader"
+require 'sinatra/base'
+require 'json'
 
-require 'diversity/registry'
+#require "sinatra/reloader"
 
-class DiversityWeb < Sinatra::Application
-  register Sinatra::Reloader
+require 'diversity'
 
-  enable :reloader
-  enable :sessions
+class DiversityWeb < Sinatra::Base #Sinatra::Application
+  #register Sinatra::Reloader
+
+  #enable :reloader, :sessions, :dump_errors
+
+  set :root, File.dirname(__FILE__)
 
   configure :development do
+    enable :dump_errors
+  end
+
+  def initialize(app = nil)
+    super
+
+    @registry = Diversity::Registry::DiversityApi.new(
+      { backend_url: "http://api.diversity.io:8181/" }
+    )
+    @local_registry = Diversity::Registry::Local.new({
+      base_path: File.join(settings.root, 'components'),
+    })
+    @engine = Diversity::Engine.new({
+                                      registry: @registry,
+                                      debug_logger: $stdout,
+                                    })
+  end
+
+  error 500 do |err|
+    puts "500"
+    puts (Time.now.strftime('%F %T ') <<
+          "#{err.class} - #{err.message}\n")
+    err.backtrace.each do |step|
+      puts "\t#{step}\n"
+    end
+    response.headers['Content-Type'] = 'text/plain'
+    halt 'Internal server error'
   end
 
   get "/" do
-    "Diversity Templating"
+    "Hej"
   end
 
-  get "/registry" do
-    registry = Diversity::Registry.new('./components', { cache: false })
+  get "*" do
+    path = params[:splat]
 
-    registry.installed_components.inspect
+    filename = File.join(settings.root, 'site', path)
+
+    halt 404 unless File.exist?(filename)
+
+
+    puts "File #{File.join(settings.root, path)} exists."
+
+    context = { "language" => "en" }
+
+    settings = JSON.parse(File.read(filename))
+
+    # @todo Registries must really have a super-registry...
+    component = @local_registry.get_component(settings['component'])
+
+    unless component
+      puts "No local component #{settings['component']}\n"
+      component = @registry.get_component(settings['component'])
+    end
+
+    @engine.render(component, context, settings['settings'])
   end
 
-  get "/layout" do
-    registry = Diversity::Registry.new('./components', { cache: false })
-    layout = registry.get_component('layout')
-    layout.render({ options: { title: "Hay!" } })
-  end
+  run! if app_file == $0
 end
 
 # Todo:
